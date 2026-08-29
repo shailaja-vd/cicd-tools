4410216f1b931c136766bc273df29948985bb3aa
shailajae <shailajaenjam0@gmail.com>
2026-08-29 23:08:15 +0530

    cicd-tools

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.37.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "sh-remote-state-dev"
+    key    = "expense-cicd" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region = "us-east-1"
+    dynamodb_table = "sh-remote-state-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
